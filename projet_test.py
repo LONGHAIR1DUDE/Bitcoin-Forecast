62ad2ba44723c0a873ca68b25c0ac95c17a5884b
walid IDIR <idirwalidhakim31@gmail.com>
2021-10-08 16:16:27 +0200

    "formattage des données"

--- projet_test.py
+++ projet_test.py
@@ -11,39 +11,61 @@ import sklearn as sk
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor
+from scipy import optimize 
 
-def convertion(tab):
+def convertion_prix(tab):
 	temp=tab.replace('.','',1)
 	temp=temp.replace(',','.',1)
 
 	return float(temp)
 
-def formating(elt):
-    for i in range(bitcoin[elt].shape[0]):
-    	bitcoin[elt][i]=convertion(bitcoin[elt][i])
-
-
-bitcoin = pd.read_csv('BTC-USD.csv',index_col='Date',parse_dates=True)
-
-
-formating('Ouv.')
-formating('Dernier')
-formating('Plus Haut')
-formating('Plus Bas')
-
-print(bitcoin.head())
-#bitcoin['2020']['Plus Haut'].plot(figsize=(15,6))
 
 def convertion_vol(elt):
    
     temp=elt.replace('K','',1)
     temp=temp.replace(',','.',1) 
-    temp=temp.replace('-','0.0',1)
-    
+    temp=temp.replace('-','0.0',1)   
     return float(temp)*1000
-for i in range(bitcoin['Vol.'].shape[0]):
-    	bitcoin['Vol.'][i]=convertion_vol(bitcoin['Vol.'][i])
      
+
+def convertion_variation(elt):
+    temp=elt.replace(',','.',1)
+    temp=temp.replace('%','',1)
+    return float(temp)
+
+#c'est moche 
+def formating(elt, dtFrame,conversion_type):
+    if conversion_type=='prix':
+        for i in range(dtFrame[elt].shape[0]):
+            dtFrame[elt][i]=convertion_prix(dtFrame[elt][i])
+
+    elif conversion_type=='volume':
+        for i in range(dtFrame[elt].shape[0]):
+            dtFrame[elt][i]=convertion_vol(dtFrame[elt][i])
+
+    elif conversion_type=='variation':
+        for i in range(dtFrame[elt].shape[0]):
+            dtFrame[elt][i]=convertion_variation(dtFrame[elt][i])
+
+
+bitcoin = pd.read_csv('BTC-USD.csv',index_col='Date',parse_dates=True)
+
+
+formating('Ouv.',bitcoin,'prix')
+formating('Dernier',bitcoin,'prix')
+formating('Plus Haut',bitcoin,'prix')
+formating('Plus Bas',bitcoin,'prix')
+formating('Vol.',bitcoin,'volume')
+formating('Variation %',bitcoin,'variation')
+
+print(bitcoin.head())
+#bitcoin['2020']['Plus Haut'].plot(figsize=(15,6))
+#bitcoin['2020']['Variation %'].plot(figsize=(15,6))
+#plt.show()
+
+
+
+
 cols_to_use = ['Dernier','Ouv.','Plus Haut','Plus Bas','Vol.']
 X = bitcoin[cols_to_use]
 # Select target
@@ -58,6 +80,6 @@ my_model.fit(X_train, y_train)
 
 
 predictions = my_model.predict(X_valid)
+print("Mean Absolute Error: " + str(mean_absolute_error(predictions, y_valid)))
 plt.plot(predictions)
 plt.show()
-print("Mean Absolute Error: " + str(mean_absolute_error(predictions, y_valid)))
