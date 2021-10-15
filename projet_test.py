fb434687a692f6ad6a7d464785d898ac212d495b
ISSAM ALLOUANI <issam.allouani@etu.univ-lyon1.fr>
2021-10-15 17:15:38 +0200

    Maj model Xgb

--- projet_test.py
+++ projet_test.py
@@ -67,7 +67,7 @@ def formating(elt, dtFrame,conversion_type):
             dtFrame[elt][i]=convertion_variation(dtFrame[elt][i])
 
 
-bitcoin = pd.read_csv('BTC-USD.csv',index_col='Date',parse_dates=True,dayfirst=True)
+bitcoin = pd.read_csv('BTC-USD.csv',index_col='Date',parse_dates=[0],dayfirst=True)
 cols_to_use = ['Dernier','Plus Haut','Plus Bas','Vol.','Variation %']
 
 
@@ -81,60 +81,85 @@ formating('Variation %',bitcoin,'variation')
 
 
 
-for i in cols_to_use:
-    bitcoin[i] = bitcoin[i].astype(float)
 
-print(bitcoin)
-print(bitcoin.dtypes)
 X = bitcoin[cols_to_use]
 # Select target
 y = bitcoin['Ouv.']
 
 
-x_train = X['2012':'2020']
-x_valid = X['2021']
+#x_train = X['2012':'2020']
+#x_valid = X['2021']
+#
+#y_train=y['2012':'2020']
+#y_valid=y['2021']
 
-y_train=y['2012':'2020']
-y_valid=y['2021']
+split_date = '2021-05-01'
+data_train = bitcoin.loc[bitcoin.index <= split_date].copy()
+data_test = bitcoin.loc[bitcoin.index > split_date].copy()
+def create_features(df, label=None):
+    """
+    Creates time series features from datetime index
+    """
+    df['date'] = df.index
+    df['Day'] = df['date'].dt.day
+    
+    df['quarter'] = df['date'].dt.quarter
+    df['month'] = df['date'].dt.month
+    df['year'] = df['date'].dt.year
+    df['Vol.'] = df['Vol.'].astype(float)
+    df['Plus Haut'] = df['Plus Haut'].astype(float)
+    df['Dernier'] = df['Dernier'].astype(float)
+    df['Plus Bas'] = df['Plus Bas'].astype(float)
+    df['Variation %'] = df['Variation %'].astype(float)
+    X = df[['Day','quarter','month','year','Vol.','Dernier','Plus Haut','Plus Bas','Variation %']]
+    if label:
+        y = df[label]
+        return X, y
+    return X
+
+X_train, y_train = create_features(data_train, label='Ouv.')
+X_test, y_test = create_features(data_test, label='Ouv.')
 
 
-
-my_model = XGBRegressor(n_estimators=1000, learning_rate=0.05)
-my_model.fit(x_train, y_train, 
-             early_stopping_rounds=10, 
-             eval_set=[(x_valid, y_valid)], 
-             verbose=False)
-
-
-
-predictions = my_model.predict(x_valid)
-print("<----------------------------------------->")
-print("Mean Absolute Error: " + str(mean_absolute_error(predictions, y_valid)))
-predictions_df = pd.Series(predictions)
-print("<----------------------------------------->")
-print("---------------------------")
-print(y_valid)
-print("---------------------------")
-print(predictions_df)
-
-
-
-
-df=pd.DataFrame(bitcoin['Ouv.'])
+df=pd.DataFrame(X_train)
 df.to_csv("output.csv")
+def XGB_model(x_train, y_train,x_valid,y_valid):
+    my_model = XGBRegressor(n_estimators=3000, learning_rate=0.01,tree_method='hist',max_bin=300,objective ='reg:squarederror',
+         alpha = 6)
+    my_model.fit(x_train, y_train, 
+         early_stopping_rounds=5, 
+         eval_set=[(x_train,y_train),(x_valid, y_valid)], 
+         verbose=False)
+    predictions = my_model.predict(x_valid)
+    print("<----------------------------------------->")
+   
+    print("Mean Absolute Error: " + str(mean_absolute_error(predictions, y_valid)))
+    return pd.Series(predictions)
+
+
+predictions_df = XGB_model(X_train,y_train,X_test,y_test)
+
+
+
 
 ax = list(bitcoin.index)
 trace1 = go.Scatter(
     x = ax,
     y= bitcoin['Ouv.'],
     mode = 'lines+markers',
-    name = 'Ouv.'
+    name = 'Open'
 )
 trace2 = go.Scatter(
     x = ax,
     y= predictions_df,
     mode = 'lines',
-    name = 'Xg_pred.'
+    name = 'XgBoost_pred_Open'
+)
+trace3 = go.Scatter(
+    x = ax,
+    y= bitcoin['Plus Haut'],
+    mode = 'lines',
+    name = 'highest'
 )
 layout = dict(
     title='Historical Bitcoin Prices (2012-2021) with the Slider ',
@@ -167,11 +192,12 @@ layout = dict(
         type='date'
     )
 )
-data = [trace1,trace2]
+data = [trace1,trace2,trace3]
 fig = go.Figure(data=data,layout=layout)
-iplot(fig, filename = "Time Series with Rangeslider") 
+#iplot(fig, filename = "Time Series with Rangeslider") 
 print("<----------------------------------------->")
 fig.write_html("output.html")
 print("Output File is Ready")
-print(y_train)
-print("<----------------------------------------->")
+df=bitcoin.astype(float)
+print(df.corr())
+sns.heatmap(df.corr(), annot=True, cmap='RdYlGn', linewidths=0.1, vmin=0)
